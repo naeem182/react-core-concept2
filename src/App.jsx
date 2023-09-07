ff011a099143a54e18c48c2b38c1b752d94ffdef
naeem <naeemislamjr182@gmail.com>
2023-09-08 01:42:22 +0600

    different way to handle function to evesnt handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  //return er baire
+  //return er baire fun
   function handleClick() {
     alert('button clicked');
+  }
+  //arrow function
+  const handleclick2 = () => {
+    alert('button click 2')
+  }
+  //parametarize function
+  function Addtofive(num) {
+    alert(num + 5)
   }
 
   return (
     <>
 
       <h3> React core concepts 2</h3>
+      {/* By function */}
       <button onClick={handleClick}>click me</button>
+      {/* By arrow function */}
+      <button onClick={handleclick2}>click 2</button>
+      {/* direct write function */}
+      <button onClick={() => alert('button third clicked')}>button third</button>
+      {/* //pass parameter in fun by arrow function */}
+      <button onClick={() => Addtofive(3)}>pass parameter in fun by arrow function</button>
+
 
 
     </>
