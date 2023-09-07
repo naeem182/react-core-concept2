8702314b186be36f77fb15d31d8386418f0da282
naeem <naeemislamjr182@gmail.com>
2023-09-08 03:07:26 +0600

    recap add amd remove (by event handler(onclick)r)

--- src/players_add_remove.jsx
+++ src/players_add_remove.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react"
+
 export default function Team() {
+
+    //set usestate
+    const [count, setcount] = useState(0)
+
+    //add handelEvent
+    const handleAdd = () => {
+
+        const addValue = count + 1;
+        setcount(addValue);
+    }
+
+    //reduce handelEvent
+    const handleReduce = () => {
+
+        const addValue = count - 1;
+        if (addValue < 0) { alert("you can't go less than 0") }
+        else
+            setcount(addValue);
+    }
+
+
+
+
+
     const playerStyle = {
         border: "2px solid purple",
         margin: "10px",
-        padding: "5px"
+        padding: "5px",
+        borderRadius: "10px"
     }
 
     return (
-        <div style={playerStyle}>Players:</div>
+        <div style={playerStyle}>
+            <h3>Players:{count}</h3>
+            <button style={{ backgroundColor: "blue" }} onClick={handleAdd}>Add</button>
+            <button style={{ backgroundColor: "red", marginLeft: '10px' }} onClick={handleReduce}>Reduce</button>
+
+        </div>
     )
 }
